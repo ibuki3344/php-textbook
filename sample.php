5665e9c85f8321b832c31edd8bcd326f434e4c3a
ibuki3344 <ibuki334455@yahoo.co.jp>
2015-05-13 04:36:34 +0000

    0513 practice

---
+++ sample.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    print('<h2 style="fontsize: 110%; font-weight: bold">画面にHTMLを表示する</h2>');
+    
+    print('<p> PHPはHTMLの中に埋め込んで記述することができます。</P>');
+    
+    print('<p>改行するときは、<br />brタグを記述します</p>');
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    print('phpを勉強中です');
+    ?>
+</body>
+</html>
